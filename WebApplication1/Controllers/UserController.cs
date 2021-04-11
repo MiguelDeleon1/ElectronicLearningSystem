a96f5f25a399b05d64e5b332a6fd25e9df9a6cc1
Miguel Deleon <77979306+MiguelDeleon1@users.noreply.github.com>
2021-04-11 18:08:43 -0500

    Initial Commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Electronic_Learning_System.Models;
+using System.Collections.Generic;
+
+
+namespace Electronic_Learning_System.Controllers
+{
+    public class UserController
+        : Controller
+    {
+        // C r e a t e
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(User Hahahaa)
+        {
+            //Adds this User(v) to the database.
+            return RedirectToAction("Details");
+            //return View("Details");
+        }
+        // R e a d
+
+        // Read a User out of the Database and display it on a web page.
+        public IActionResult Details(int id)
+        {
+            // 1. Go to the database and retrieve User with the given id.
+            User v = new User();
+            v.DODID = 1;
+            v.Password = "apples";
+            v.ConfirmPassword = "apples";
+            v.FirstName = "Miguel";
+            v.LastName = "Deleon";
+            v.Occupation = "HR";
+            v.Title = "SGT";
+
+            // 2. Display that User on a View.
+           return View(v);
+
+        }
+
+        // U p d a t e
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // 1. Go to the database and retrieve User with the given id.
+            User v = new User();
+            v.DODID = 1;
+            v.Password = "SolarWinds123-lol";
+            v.ConfirmPassword = "SolarWinds123-lol";
+            v.FirstName = "Miguel";
+            v.LastName = "Deleon";
+            v.Occupation = "HR";
+            v.Title = "SGT";
+
+            // 2. Display that User on a View.
+            return View(v);
+        }
+        [HttpPost]
+        public IActionResult Edit(User v)
+        {
+            //Update the User(v) in the database - ignore for now
+            return RedirectToAction("Details");
+        }
+
+        // D e l e t e
+
+
+
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Electronic_Learning_System.Models
+{
+    public class User
+    {
+        // F i e l d s & P r o p e r t i e s
+        public int DODID { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Occupation { get; set; }
+        public string Title { get; set; }
+
+        // C o n s t r u c t o r s
+
+        // M e t h o d s
+
+    }
+}
